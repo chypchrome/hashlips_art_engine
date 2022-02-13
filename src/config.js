06995520fa34db7cc4ebc04552577e3666648847
Matthew Chun <chypchrome@Matthews-MBP-2.home>
2022-02-12 18:55:27 -1000

    remove layers, keep one as commented for syntax

--- src/config.js
+++ src/config.js
@@ -14,13 +14,7 @@ const layerConfigurations = [
   {
     growEditionSizeTo: 5,
     layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      // { name: "Background" },
     ],
   },
 ];
